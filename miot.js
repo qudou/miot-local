1c85a05701932df3ec6aa16f9cd5f8ccfc8908af
qudou <qudou@xmlplus.cn>
2018-02-18 18:12:39 +0800

    update

---
+++ miot.js
@@ -0,0 +1,133 @@
+/*!
+ * miot.js v1.0.0
+ * https://github.com/qudou/miot
+ * (c) 2009-2017 qudou
+ * Released under the MIT license
+ */
+
+const mosca = require('mosca');
+const xmlplus = require("xmlplus");
+const ID = "aee81434-fe5f-451a-b522-ae4631da5f45";
+const Gateway = "c55d5e0e-f506-4933-8962-c87932e0bc2a";
+
+xmlplus("miot", (xp, $_, t) => {
+
+$_().imports({
+    Index: {
+        xml: "<main id='index'>\
+                <Mosca id='mosca'/>\
+                <Proxy id='proxy'/>\
+              </main>",
+        map: { share: "sqlite/Sqlite Parts" }
+    },
+    Mosca: {
+        xml: "<Parts id='parts'/>",
+        fun: async function (sys, items, opts) {
+            let options = await items.parts.data();
+            let server = new mosca.Server({port: 1883});
+            server.on("ready", () => {
+                console.log("Mosca server is up and running"); 
+            });
+            server.on("subscribed", async (topic, client) => {
+                let data = options[topic].data;
+                items.parts.update(topic, 1);
+                this.notify("answer", {ssid: topic, online: 1, data: data});
+                this.notify("publish", [topic, {topic: "message", body: data}]);
+            });
+            server.on("unsubscribed", (topic, client) => {
+                items.parts.update(topic, 0);
+                this.notify("answer", {ssid: topic, online: 0});
+            });
+            server.on("published", async (packet, client) => {
+                if (packet.topic == ID) {
+                    let o = JSON.parse(packet.payload + '');
+                    xp.extend(options[o.ssid].data, o.data);
+                    items.parts.cache(o.ssid, options[o.ssid].data);
+                    this.notify("answer", {ssid: o.ssid, data: o.data});
+                }
+            });
+            this.watch("publish", (e, topic, msg) => {
+                server.publish({topic: topic, payload: JSON.stringify(msg), qos: 1, retain: false});
+            });
+        }
+    },
+    Proxy: {
+        xml: "<Parts id='parts'/>",
+        opt: { server: "mqtt://t-store.cn:1888", clientId: ID },
+        fun: function (sys, items, opts) {
+            let client  = require("mqtt").connect(opts.server, opts);
+            client.on("connect", async e => {
+                client.subscribe(opts.clientId);
+                let data = await items.parts.data();
+                for (let item in data)
+                    this.notify("answer", {ssid: item.ssid, online: item.online, data: item.data});
+                console.log("connected to " + opts.server);
+            });
+            client.on("message", (topic, msg) => {
+                msg = JSON.parse(msg.toString());
+                this.notify("publish", [msg.ssid, msg.body]);
+            });
+            this.watch("answer", (e, payload) => {
+                client.publish(Gateway, JSON.stringify(payload), {qos: 1, retain: false});
+            });
+        }
+    },
+    Parts: {
+        xml: "<Sqlite id='sqlite' xmlns='/sqlite'/>",
+        fun: function (sys, items, opts) {
+            function data() {
+                return new Promise(resolve => {
+                    items.sqlite.all("SELECT * FROM parts", (err, rows) => {
+                        if (err) throw err;
+                        let table = {};
+                        rows.forEach(item => {
+                            item.ssid = item.id;
+                            delete item.id;
+                            item.data = JSON.parse(item.data);
+                            table[item.ssid] = item;
+                        });
+                        resolve(table);
+                    });
+                });
+            }
+            function update(id, online) {
+                let stmt = items.sqlite.prepare("UPDATE parts SET online=? WHERE id=?");
+                stmt.run(online, id, err => {
+                    if (err) throw err;
+                });
+            }
+            function cache(id, data) {
+                let stmt = items.sqlite.prepare("INSERT Or REPLACE INTO parts VALUES (?,?,?)");
+                stmt.run(id, JSON.stringify(data), 1, err => {
+                    if (err) throw err;
+                });
+            }
+            return { data: data, update: update, cache: cache };
+        }
+    }
+});
+
+$_("sqlite").imports({
+    Sqlite: {
+        fun: function (sys, items, opts) {
+            let sqlite = require("sqlite3").verbose(),
+                db = new sqlite.Database(`${__dirname}/data.db`);
+			db.exec("VACUUM");
+            db.exec("PRAGMA foreign_keys = ON");
+            return db;
+        }
+    },
+    Prepare: {
+        fun: function (sys, items, opts) {
+            return stmt => {
+                let args = [].slice.call(arguments).slice(1);
+                args.forEach(item => {
+                    stmt = stmt.replace("?", typeof item == "string" ? '"' + item + '"' : item);
+                });
+                return stmt;
+            };
+        }
+    }
+});
+
+}).startup("//miot/Index");
