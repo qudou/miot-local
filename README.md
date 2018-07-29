14c882f2b636063bd1f8c07eac3033784aae81a9
qudou <qudou@xmlplus.cn>
2018-07-29 13:20:34 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # miot-local
 
 miot 物联网平台的局域网网关。
+
+## 安装 samba
+
+sh```
+sudo apt-get -y install samba
+```
+
+安装 samba 后，网内的配件可以通过计算机名 raspberrypi 连接网关。
--- miot-local.js
+++ miot-local.js
@@ -18,7 +18,7 @@ $_().imports({
                 <Mosca id='mosca'/>\
                 <Proxy id='proxy'/>\
               </main>",
-        map: { share: "sqlite/Sqlite mosca/Links mosca/Parts" }
+        map: { share: "mosca/Sqlite mosca/Links mosca/Parts" }
     },
     Mosca: {
         xml: "<main id='mosca' xmlns:i='mosca'>\
@@ -27,7 +27,7 @@ $_().imports({
               </main>",
         fun: async function (sys, items, opts) {
             let options = await items.parts.data();
-            let server = new mosca.Server({port: 1883});            
+            let server = new mosca.Server({port: 1883});
             server.on("ready", async () => {
                 await items.parts.offlineAll();
                 Object.keys(items.auth).forEach(k => server[k] = items.auth[k]);
@@ -35,15 +35,15 @@ $_().imports({
             });
             server.on("subscribed", async (topic, client) => {
                 let data = options[topic].data;
-                items.parts.update(topic, 1);
+                await items.parts.update(topic, 1);
                 this.notify("to-part", [topic, {topic: "message", body: data}]);
                 this.notify("to-gateway", {ssid: topic, online: 1, data: data});
             });
-            server.on("unsubscribed", (topic, client) => {
-                items.parts.update(topic, 0);
+            server.on("unsubscribed", async (topic, client) => {
+                await items.parts.update(topic, 0);
                 this.notify("to-gateway", {ssid: topic, online: 0});
             });
-            server.on("published", async (packet, client) => {
+            server.on("published", (packet, client) => {
                 if (client == undefined) return;
                 if (packet.topic == "to-gateway") {
                     let msg = JSON.parse(packet.payload + '');
@@ -59,7 +59,7 @@ $_().imports({
     },
     Proxy: {
         xml: "<Parts id='parts' xmlns='mosca'/>",
-        opt: { server: "mqtt://t-store.cn:1883", clientId: ID },
+        opt: { server: "mqtt://xmlplus.cn:1883", clientId: ID },
         fun: function (sys, items, opts) {
             let client  = require("mqtt").connect(opts.server, opts);
             client.on("connect", async e => {
@@ -98,12 +98,12 @@ $_("mosca").imports({
         }
     },
     Links: {
-        xml: "<Sqlite id='sqlite' xmlns='/sqlite'/>",
+        xml: "<Sqlite id='db'/>",
         fun: function (sys, items, opts) {
             function canLink(linkId) {
                 return new Promise((resolve, reject) => {
                     let stmt = `SELECT * FROM links WHERE id = '${linkId}'`;
-                    items.sqlite.all(stmt, (err, data) => {
+                    items.db.all(stmt, (err, data) => {
                         if (err) throw err;
                         resolve(!!data.length);
                     });
@@ -113,12 +113,12 @@ $_("mosca").imports({
         }
     },
     Parts: {
-        xml: "<Sqlite id='sqlite' xmlns='/sqlite'/>",
+        xml: "<Sqlite id='db'/>",
         fun: function (sys, items, opts) {
             function canSubscribe(partId) {
                 return new Promise((resolve, reject) => {
                     let stmt = `SELECT parts.* FROM links, parts WHERE parts.id = '${partId}' AND parts.link = links.id AND parts.online = 0`;
-                    items.sqlite.all(stmt, (err, data) => {
+                    items.db.all(stmt, (err, data) => {
                         if (err) throw err;
                         resolve(!!data.length);
                     });
@@ -126,7 +126,7 @@ $_("mosca").imports({
             }
             function data() {
                 return new Promise(resolve => {
-                    items.sqlite.all("SELECT * FROM parts", (err, rows) => {
+                    items.db.all("SELECT * FROM parts", (err, rows) => {
                         if (err) throw err;
                         let table = {};
                         rows.forEach(item => {
@@ -140,20 +140,23 @@ $_("mosca").imports({
                 });
             }
             function update(id, online) {
-                let stmt = items.sqlite.prepare("UPDATE parts SET online=? WHERE id=?");
-                stmt.run(online, id, err => {
-                    if (err) throw err;
+                return new Promise(resolve => {
+                    let stmt = items.db.prepare("UPDATE parts SET online=? WHERE id=?");
+                    stmt.run(online, id, err => {
+                        if (err) throw err;
+                        resolve(true);
+                    });
                 });
             }
             function cache(id, data) {
-                let stmt = items.sqlite.prepare("UPDATE parts set data = ? WHERE id = ?");
+                let stmt = items.db.prepare("UPDATE parts set data = ? WHERE id = ?");
                 stmt.run(JSON.stringify(data), id, err => {
                     if (err) throw err;
                 });
             }
             function offlineAll() {
                 return new Promise((resolve, reject) => {
-                    let stmt = items.sqlite.prepare("UPDATE parts SET online=?");
+                    let stmt = items.db.prepare("UPDATE parts SET online=?");
                     stmt.run(0, err => {
                         if (err) throw err;
                         resolve(true);
@@ -162,10 +165,7 @@ $_("mosca").imports({
             }
             return { canSubscribe: canSubscribe, data: data, update: update, cache: cache, offlineAll: offlineAll };
         }
-    }
-});
-
-$_("sqlite").imports({
+    },
     Sqlite: {
         fun: function (sys, items, opts) {
             let sqlite = require("sqlite3").verbose(),
@@ -173,17 +173,6 @@ $_("sqlite").imports({
             db.exec("VACUUM");
             db.exec("PRAGMA foreign_keys = ON");
             return db;
-        }
-    },
-    Prepare: {
-        fun: function (sys, items, opts) {
-            return stmt => {
-                let args = [].slice.call(arguments).slice(1);
-                args.forEach(item => {
-                    stmt = stmt.replace("?", typeof item == "string" ? '"' + item + '"' : item);
-                });
-                return stmt;
-            };
         }
     }
 });
